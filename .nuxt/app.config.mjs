eb6bd8f328ac3984dcb418423c31ffc9b3eb61af
Vitaly <vitalic.web@gmail.com>
2023-05-21 17:43:30 +0300

    Fix git ignore

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/vitalijstekolsikov/Desktop/dev/vue-school/vue-school-chat-bot-gpt/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
